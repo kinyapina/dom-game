29c65ee8caa7e58f39b00b976d8eecf33a8c7b01
Yekaterina Kinyapina <kinyapina.katya28@gmail.com>
2026-03-24 09:18:57 +0700

    added js components

---
+++ src/js/gameField.js
@@ -0,0 +1,49 @@
+export default class GameField {
+  constructor(size = 4) {
+    this.size = size; // 4x4
+    this.cells = [];
+    this.element = null;
+    this.onCellClick = null; // колбэк при клике на ячейку
+  }
+
+  // Создаем игровое поле
+  render() {
+    this.element = document.createElement('div');
+    this.element.className = 'field';
+
+    // Создаем ячейки
+    for (let i = 0; i < this.size * this.size; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.dataset.index = i;
+
+      // Добавляем обработчик клика
+      cell.addEventListener('click' (event) => {
+        if (this.onCellClick) {
+          this.onCellClick(cell, i);
+        }
+      });
+
+      this.element.appendChild(cell);
+      this.cells.push(cell);
+    }
+
+    return this.element;
+  }
+
+    
+  // Получить ячейку по индексу
+  getCell(index) {
+    return this.cells;
+  }
+
+  // Получить все ячейки
+  getAllCells() {
+    this.cells.forEach(cell => {
+      const goblin = cell.querySelector('.goblin');
+      if (goblin) {
+        cell.removeChild(goblin);
+      }
+    });
+  }
+}
---
+++ src/js/goblin.js
@@ -0,0 +1,35 @@
+export class default Goblin {
+  constructor() {
+    this.element = null;
+    this.currentCell = null;
+  }
+
+  // Создаем гоблина
+  render() {
+    this.element = document.createElement('img');
+    this.element.src = 'https://raw.githubusercontent.com/netology-code/ahj-homeworks/AHJ-50/dom/pic/goblin.png';
+    this.element.classList.add('goblin');
+    this.element.alt = 'Goblin';
+    return this.element;
+  }
+
+  // Поместить гоблина в ячейку
+  placeInCell(cell) {
+    if (!cell) return;
+
+    // Если гоблин уже в какой-то ячейке, он автоматически переместится
+    // appendChild сам удалит из старого места
+    cell.appendChild(this.element);
+    this.currentCell = cell;
+  }
+
+  // Получить текущую ячейку
+  getCurrentCell() {
+    return this.currentCell;
+  }
+
+  // Проверить, находится ли гоблин в указанной ячейке
+  isInCell(cell) {
+    return this.currentCell === cell;
+  }
+}
